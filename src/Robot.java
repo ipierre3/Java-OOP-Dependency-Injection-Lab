9eae2fd77d52ca18b086177539426c55aecb78dd
Ignace Pierre <ipierre354@gmail.com>
2022-11-09 16:19:34 -0500

    Deleted hardcoded method & implemented depency injection in Robot Class

--- src/Robot.java
+++ src/Robot.java
@@ -2,18 +2,17 @@ public class Robot {
 
     //Instance variables (HAS A)
     public String name;
-    public String weaponName;
-    public int weaponPower;
+    public Weapon weapon;
 
     //Constructor
     public Robot() {
-        this.name = "T-1000";
-        this.weaponName = "Gun";
-        this.weaponPower = 50;
+        this.name = "AstroBoy";
+        this.weapon = new Weapon("Gun", 50);
+
     }
 
     //Methods (CAN DO)
     public void Attack() {
-        System.out.println(this.name + " attacks with their " + this.weaponName + " for " + this.weaponPower + " damage ");
+        System.out.println(this.name + " attacks with his " + this.weapon.name + " for " + this.weapon.power + " damage ");
     }
 }
---
+++ src/Weapon.java
@@ -0,0 +1,12 @@
+public class Weapon {
+    //Instance variables (HAS A)
+    public  String name;
+    public int power;
+
+    //Constructor
+    public Weapon(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+}
