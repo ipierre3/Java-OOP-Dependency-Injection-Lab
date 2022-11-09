4f003c134121017e635fda9bbde3141e26a08da7
Ignace Pierre <ipierre354@gmail.com>
2022-11-09 16:35:47 -0500

    Dependency Injection ACHIEVEDgit add .!

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Weapon gun = new Weapon("Gun", 50);
-        Robot robot_one = new Robot(gun);
+//        Weapon gun = new Weapon("Gun", 50);
+        Weapon club = new Weapon("Club", 25);
+        Robot robot_one = new Robot(club);
         System.out.println(robot_one.name);
         robot_one.Attack();
 
